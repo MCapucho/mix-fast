fdd0918b7a0ebe6fd53d16900b3670fce8910966
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-06-21 00:49:07 -0300

    Implementação do projeto

--- src/main/java/br/com/postech/mixfast/entrypoints/controller/v1/cliente/ClienteController.java
+++ src/main/java/br/com/postech/mixfast/entrypoints/controller/v1/cliente/ClienteController.java
@@ -4,6 +4,9 @@ import br.com.postech.mixfast.core.entity.Cliente;
 import br.com.postech.mixfast.core.usecase.interfaces.cliente.*;
 import br.com.postech.mixfast.entrypoints.http.ClienteHttp;
 import br.com.postech.mixfast.entrypoints.http.mapper.ClienteHttpMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -28,6 +31,11 @@ public class ClienteController {
     private final ClienteDeletarPorCodigoUseCase clienteDeletarPorCodigoUseCase;
     private final ClienteBuscarPorCpfUseCase clienteBuscarPorCpfUseCase;
 
+    @Operation(summary = "Cadastrar um novo cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente cadastrada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar um novo cliente com os dados informados"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @PostMapping
     public ResponseEntity<ClienteHttp> cadastrar(@Valid @RequestBody ClienteHttp clienteHttp) {
         Cliente cliente = clienteCadastrarUseCase.cadastrar(clienteHttpMapper.httpToEntity(clienteHttp));
@@ -35,6 +43,11 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteHttpMapper.entityToHttp(cliente));
     }
 
+    @Operation(summary = "Buscar todos clientes cadastrados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de clientes preenchida com sucesso"),
+            @ApiResponse(responseCode = "204", description = "Lista de clientes em branco"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping
     public ResponseEntity<List<ClienteHttp>> buscarTodos() {
         List<Cliente> listaClientes = clienteBuscarTodosUseCase.buscarTodos();
@@ -49,6 +62,11 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.OK).body(listaClientesHttp);
     }
 
+    @Operation(summary = "Buscar um cliente cadastrado por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado com o código informado"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping("/{codigo}")
     public ResponseEntity<ClienteHttp> buscarPorCodigo(@PathVariable("codigo") String codigo) {
         Cliente cliente = clienteBuscarPorCodigoUseCase.buscarPorCodigo(codigo);
@@ -56,6 +74,11 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.OK).body(clienteHttpMapper.entityToHttp(cliente));
     }
 
+    @Operation(summary = "Atualizar um cliente cadastrado por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado com o código informado"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @PutMapping("/{codigo}")
     public ResponseEntity<ClienteHttp> atualizar(@PathVariable("codigo") String codigo,
                                                  @Valid @RequestBody ClienteHttp clienteHttp) {
@@ -64,6 +87,11 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.OK).body(clienteHttpMapper.entityToHttp(cliente));
     }
 
+    @Operation(summary = "Deletar um cliente cadastrado por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado com o código informado"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @DeleteMapping("/{codigo}")
     public ResponseEntity<Void> deletarPorCodigo(@PathVariable("codigo") String codigo) {
         clienteDeletarPorCodigoUseCase.deletarPorCodigo(codigo);
@@ -71,6 +99,11 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Buscar um cliente cadastrado por CPF")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado com o CPF informado"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping("/cpf/{cpf}")
     public ResponseEntity<ClienteHttp> buscarPorCpf(@PathVariable("cpf") String cpf) {
         Cliente cliente = clienteBuscarPorCpfUseCase.buscarPorCpf(cpf);
--- src/main/java/br/com/postech/mixfast/entrypoints/controller/v1/produto/ProdutoController.java
+++ src/main/java/br/com/postech/mixfast/entrypoints/controller/v1/produto/ProdutoController.java
@@ -4,6 +4,9 @@ import br.com.postech.mixfast.core.entity.Produto;
 import br.com.postech.mixfast.core.usecase.interfaces.produto.*;
 import br.com.postech.mixfast.entrypoints.http.ProdutoHttp;
 import br.com.postech.mixfast.entrypoints.http.mapper.ProdutoHttpMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -28,6 +31,11 @@ public class ProdutoController {
     private final ProdutoDeletarPorCodigoUseCase produtoDeletarPorCodigoUseCase;
     private final ProdutoBuscarPorCategoriaUseCase produtoBuscarPorCategoriaUseCase;
 
+    @Operation(summary = "Cadastrar um novo produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Produto cadastrada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar um novo produto com os dados informados"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @PostMapping
     public ResponseEntity<ProdutoHttp> cadastrar(@Valid @RequestBody ProdutoHttp produtoHttp) {
         Produto produto = produtoCadastrarUseCase.cadastrar(produtoHttpMapper.httpToEntity(produtoHttp));
@@ -35,6 +43,11 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoHttpMapper.entityToHttp(produto));
     }
 
+    @Operation(summary = "Buscar todos produtos cadastrados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de produtos preenchida com sucesso"),
+            @ApiResponse(responseCode = "204", description = "Lista de produtos em branco"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping
     public ResponseEntity<List<ProdutoHttp>> buscarTodos() {
         List<Produto> listaProdutos = produtoBuscarTodosUseCase.buscarTodos();
@@ -45,10 +58,15 @@ public class ProdutoController {
             listaProdutosHttp.add(produtoHttp);
         });
 
-        log.info("Lista de produto preenchida com sucesso");
+        log.info("Lista de produtos preenchida com sucesso");
         return ResponseEntity.status(HttpStatus.OK).body(listaProdutosHttp);
     }
 
+    @Operation(summary = "Buscar um produto cadastrado por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado com o código informado"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping("/{codigo}")
     public ResponseEntity<ProdutoHttp> buscarPorCodigo(@PathVariable("codigo") String codigo) {
         Produto produto = produtoBuscarPorCodigoUseCase.buscarPorCodigo(codigo);
@@ -56,6 +74,11 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.OK).body(produtoHttpMapper.entityToHttp(produto));
     }
 
+    @Operation(summary = "Atualizar um produto cadastrado por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado com o código informado"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @PutMapping("/{codigo}")
     public ResponseEntity<ProdutoHttp> atualizar(@PathVariable("codigo") String codigo,
                                                  @Valid @RequestBody ProdutoHttp produtoHttp) {
@@ -64,6 +87,11 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.OK).body(produtoHttpMapper.entityToHttp(produto));
     }
 
+    @Operation(summary = "Deletar um produto cadastrado por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado com o código informado"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @DeleteMapping("/{codigo}")
     public ResponseEntity<Void> deletarPorCodigo(@PathVariable("codigo") String codigo) {
         produtoDeletarPorCodigoUseCase.deletarPorCodigo(codigo);
@@ -71,6 +99,11 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Buscar todos produtos por categoria cadastrados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de produtos por categoria preenchida com sucesso"),
+            @ApiResponse(responseCode = "204", description = "Lista de produtos por categoria em branco"),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping("/categoria/{categoria_codigo}")
     public ResponseEntity<List<ProdutoHttp>> buscarPorCategoria(@PathVariable("categoria_codigo") String categoria) {
         List<Produto> listaProdutos = produtoBuscarPorCategoriaUseCase.buscarPorCategoria(categoria);
@@ -81,7 +114,7 @@ public class ProdutoController {
             listaProdutosHttp.add(produtoHttp);
         });
 
-        log.info("Lista de produto preenchida com sucesso");
+        log.info("Lista de produtos preenchida com sucesso");
         return ResponseEntity.status(HttpStatus.OK).body(listaProdutosHttp);
     }
 }
