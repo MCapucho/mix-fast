95f22947063cc5a8976a7d7976f2d553adc0f42c
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-06-22 02:52:50 -0300

    Implementação do projeto

--- src/main/java/br/com/postech/mixfast/entrypoints/http/PedidoHttp.java
+++ src/main/java/br/com/postech/mixfast/entrypoints/http/PedidoHttp.java
@@ -2,7 +2,6 @@ package br.com.postech.mixfast.entrypoints.http;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
