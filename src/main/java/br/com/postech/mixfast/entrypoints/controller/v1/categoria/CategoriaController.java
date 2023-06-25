69b0165e9d7565faa4b9d4dd682def3047b5c449
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-06-25 19:05:24 -0300

    Implementação do projeto

--- src/main/java/br/com/postech/mixfast/entrypoints/controller/v1/categoria/CategoriaController.java
+++ src/main/java/br/com/postech/mixfast/entrypoints/controller/v1/categoria/CategoriaController.java
@@ -2,15 +2,9 @@ package br.com.postech.mixfast.entrypoints.controller.v1.categoria;
 
 import br.com.postech.mixfast.core.entity.Categoria;
 import br.com.postech.mixfast.core.usecase.interfaces.categoria.*;
-import br.com.postech.mixfast.entrypoints.handler.ErrorResponse;
+import br.com.postech.mixfast.entrypoints.docs.CategoriaDocumentable;
 import br.com.postech.mixfast.entrypoints.http.CategoriaHttp;
 import br.com.postech.mixfast.entrypoints.http.mapper.CategoriaHttpMapper;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -20,12 +14,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "Categorias")
+
 @RequiredArgsConstructor
 @Log4j2
 @RestController
 @RequestMapping(value = "/v1/categorias")
-public class CategoriaController {
+public class CategoriaController implements CategoriaDocumentable {
 
     private final CategoriaHttpMapper categoriaHttpMapper;
     private final CategoriaCadastrarUseCase categoriaCadastrarUseCase;
@@ -34,17 +28,6 @@ public class CategoriaController {
     private final CategoriaAtualizarUseCase categoriaAtualizarUseCase;
     private final CategoriaDeletarPorCodigoUseCase categoriaDeletarPorCodigoUseCase;
 
-    @Operation(summary = "Cadastrar uma nova categoria")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Categoria cadastrada com sucesso",
-                    content = { @Content(schema = @Schema(implementation = CategoriaHttp.class),
-                            mediaType = "application/json") }),
-            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar uma nova categoria com os dados informados",
-                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
-                            mediaType = "application/json") }),
-            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados",
-                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
-                            mediaType = "application/json") })})
     @PostMapping
     public ResponseEntity<CategoriaHttp> cadastrar(@Valid @RequestBody CategoriaHttp categoriaHttp) {
         Categoria categoria = categoriaCadastrarUseCase.cadastrar(categoriaHttpMapper.httpToEntity(categoriaHttp));
@@ -52,11 +35,6 @@ public class CategoriaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaHttpMapper.entityToHttp(categoria));
     }
 
-    @Operation(summary = "Buscar todas categorias cadastradas")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de categorias preenchida com sucesso"),
-            @ApiResponse(responseCode = "204", description = "Lista de categorias em branco"),
-            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping
     public ResponseEntity<List<CategoriaHttp>> buscarTodas() {
         List<Categoria> listaCategoria = categoriaBuscarTodasUseCase.buscarTodas();
@@ -64,11 +42,6 @@ public class CategoriaController {
         return ResponseEntity.status(HttpStatus.OK).body(categoriaHttpMapper.entityListToHttpList(listaCategoria));
     }
 
-    @Operation(summary = "Buscar uma categoria cadastrada por código")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Categoria não encontrada com o código informado"),
-            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @GetMapping("/{codigo}")
     public ResponseEntity<CategoriaHttp> buscarPorCodigo(@PathVariable("codigo") String codigo) {
         Categoria categoria = categoriaBuscarPorCodigoUseCase.buscarPorCodigo(codigo);
@@ -76,11 +49,6 @@ public class CategoriaController {
         return ResponseEntity.status(HttpStatus.OK).body(categoriaHttpMapper.entityToHttp(categoria));
     }
 
-    @Operation(summary = "Atualizar uma categoria cadastrada por código")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Categoria atualizada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Categoria não encontrada com o código informado"),
-            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @PutMapping("/{codigo}")
     public ResponseEntity<CategoriaHttp> atualizar(@PathVariable("codigo") String codigo,
                                                    @Valid @RequestBody CategoriaHttp categoriaHttp) {
@@ -89,11 +57,6 @@ public class CategoriaController {
         return ResponseEntity.status(HttpStatus.OK).body(categoriaHttpMapper.entityToHttp(categoria));
     }
 
-    @Operation(summary = "Deletar uma categoria cadastrada por código")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Categoria deletada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Categoria não encontrada com o código informado"),
-            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados")})
     @DeleteMapping("/{codigo}")
     public ResponseEntity<Void> deletarPorCodigo(@PathVariable("codigo") String codigo) {
         categoriaDeletarPorCodigoUseCase.deletarPorCodigo(codigo);
---
+++ src/main/java/br/com/postech/mixfast/entrypoints/docs/CategoriaDocumentable.java
@@ -0,0 +1,87 @@
+package br.com.postech.mixfast.entrypoints.docs;
+
+import br.com.postech.mixfast.entrypoints.handler.ErrorResponse;
+import br.com.postech.mixfast.entrypoints.http.CategoriaHttp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Tag(name = "Categorias")
+public interface CategoriaDocumentable {
+
+    @Operation(summary = "Cadastrar uma nova categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Categoria cadastrada com sucesso",
+                    content = { @Content(schema = @Schema(implementation = CategoriaHttp.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar uma nova categoria com os dados informados",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") })})
+    ResponseEntity<CategoriaHttp> cadastrar(@Parameter CategoriaHttp categoriaHttp);
+
+    @Operation(summary = "Buscar todas categorias cadastradas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de categorias preenchida com sucesso",
+                    content = { @Content(array = @ArraySchema(schema = @Schema(implementation = CategoriaHttp.class)),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "204", description = "Lista de categorias em branco",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") })})
+    ResponseEntity<List<CategoriaHttp>> buscarTodas();
+
+    @Operation(summary = "Buscar uma categoria cadastrada por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso",
+                    content = { @Content(schema = @Schema(implementation = CategoriaHttp.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada com o código informado",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") })})
+    ResponseEntity<CategoriaHttp> buscarPorCodigo(@Parameter(name = "codigo", description = "Código da Categoria",
+            example = "77b36beb-68cd-4939-9911-fe92a79cff99") String codigo);
+
+    @Operation(summary = "Atualizar uma categoria cadastrada por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria atualizada com sucesso",
+                    content = { @Content(schema = @Schema(implementation = CategoriaHttp.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada com o código informado",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") })})
+    ResponseEntity<CategoriaHttp> atualizar(@Parameter(name = "codigo", description = "Código da Categoria",
+            example = "77b36beb-68cd-4939-9911-fe92a79cff99") String codigo, @Parameter CategoriaHttp categoriaHttp);
+
+    @Operation(summary = "Deletar uma categoria cadastrada por código")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria deletada com sucesso",
+                    content = { @Content(schema = @Schema(implementation = CategoriaHttp.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada com o código informado",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "409", description = "Erro na comunicação com o banco de dados",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json") })})
+    ResponseEntity<Void> deletarPorCodigo(@Parameter(name = "codigo", description = "Código da Categoria",
+            example = "77b36beb-68cd-4939-9911-fe92a79cff99") String codigo);
+}
