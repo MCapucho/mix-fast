6ffa127de6d2bea1fef2d035df59b975b89ea8be
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-06-26 22:49:53 -0300

    Implementação do projeto

--- src/main/java/br/com/postech/mixfast/core/usecase/impl/pedido/PedidoEmitirUseCaseImpl.java
+++ src/main/java/br/com/postech/mixfast/core/usecase/impl/pedido/PedidoEmitirUseCaseImpl.java
@@ -2,7 +2,6 @@ package br.com.postech.mixfast.core.usecase.impl.pedido;
 
 import br.com.postech.mixfast.core.entity.Pedido;
 import br.com.postech.mixfast.core.exception.pedido.PedidoFailedException;
-import br.com.postech.mixfast.core.gateway.PagamentoGateway;
 import br.com.postech.mixfast.core.gateway.PedidoGateway;
 import br.com.postech.mixfast.core.usecase.interfaces.pedido.PedidoEmitirUseCase;
 import lombok.RequiredArgsConstructor;
@@ -13,14 +12,10 @@ import org.springframework.stereotype.Service;
 public class PedidoEmitirUseCaseImpl implements PedidoEmitirUseCase {
 
     private final PedidoGateway pedidoGateway;
-    private final PagamentoGateway pagamentoGateway;
 
     @Override
     public Pedido emitir(Pedido pedido) {
         Pedido pedidoEmitido = pedidoGateway.emitir(pedido);
-
-//        String qrCode = pagamentoGateway.pagarQrCode(pedidoEmitido);
-//        pedidoEmitido.setQrCode(qrCode);
 
         if (pedidoEmitido.getFila() == null) {
             throw new PedidoFailedException("Erro ao emitir o pedido informado");
