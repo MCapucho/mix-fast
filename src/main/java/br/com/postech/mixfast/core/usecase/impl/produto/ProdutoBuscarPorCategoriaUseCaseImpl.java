1061d701bc30595547f764bc0ea2bf4c6b064720
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-06-27 23:54:42 -0300

    Implementação do projeto

--- src/main/java/br/com/postech/mixfast/core/usecase/impl/produto/ProdutoBuscarPorCategoriaUseCaseImpl.java
+++ src/main/java/br/com/postech/mixfast/core/usecase/impl/produto/ProdutoBuscarPorCategoriaUseCaseImpl.java
@@ -3,6 +3,7 @@ package br.com.postech.mixfast.core.usecase.impl.produto;
 import br.com.postech.mixfast.core.entity.Produto;
 import br.com.postech.mixfast.core.exception.produto.ProdutoListEmptyException;
 import br.com.postech.mixfast.core.gateway.ProdutoGateway;
+import br.com.postech.mixfast.core.usecase.interfaces.categoria.CategoriaBuscarPorCodigoUseCase;
 import br.com.postech.mixfast.core.usecase.interfaces.produto.ProdutoBuscarPorCategoriaUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,12 @@ import java.util.List;
 public class ProdutoBuscarPorCategoriaUseCaseImpl implements ProdutoBuscarPorCategoriaUseCase {
 
     private final ProdutoGateway produtoGateway;
+    private final CategoriaBuscarPorCodigoUseCase categoriaBuscarPorCodigoUseCase;
 
     @Override
     public List<Produto> buscarPorCategoria(String categoria) {
+        categoriaBuscarPorCodigoUseCase.buscarPorCodigo(categoria);
+
         List<Produto> listaProdutos = produtoGateway.buscarPorCategoria(categoria);
 
         if (listaProdutos.isEmpty()) {
