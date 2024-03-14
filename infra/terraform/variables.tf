28feba6ed317025f36615cfccdd1cceefadfef58
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-03-14 20:28:29 -0300

    Implementação do projeto

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -16,11 +16,11 @@ variable "name" {
 }
 
 variable "vpc_id" {
-  default = "vpc-0a928f18f52e3f526"
+  default = "vpc-037ba6b40e5c0940e"
 }
 variable "subnet_ids" {
   type = list
-  default = ["subnet-0f1c1df7d6204cd14", "subnet-02360cad76c9230ab", "subnet-055459a695e1caddb"]
+  default = ["subnet-06f7a7892cd3eff1e", "subnet-00cba77ae10a02240", "subnet-0b2aecbce1649de68"]
 }
 variable "network_mode" {
   type = string
@@ -36,7 +36,7 @@ variable "memory" {
 }
 variable "target_group_arn" {
   type = string
-  default = "arn:aws:elasticloadbalancing:us-east-1:022874923015:targetgroup/mixfast-target-group/2a102fd3fd0daa69"
+  default = "arn:aws:elasticloadbalancing:us-east-1:211125470560:targetgroup/mixfast-target-group/b5e774d9668bae48"
 }
 variable "port" {
   type = number
--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "mixfast-s3-bucket-app"
+    bucket = "fiap-mixfast-s3-bucket-app"
     key    = "mixfast-infrastructure"
     region = "us-east-1"
   }
